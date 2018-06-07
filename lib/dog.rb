70825106a46baa0d7c0dea86b269d30618c4abfb
meredith winchell <meredithwinchell@gmail.com>
2018-06-07 18:48:13 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,9 @@
 # dog.rb
+class Dog
+  def initialize
+  end
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,7 @@
-# person.rb
+class Person
+  def initialize
+  end
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
